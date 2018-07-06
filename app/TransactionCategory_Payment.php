d798da3a75e77f0eaa671dc151ce741446061545
itsaninho <itsaninho@gmail.com>
2018-07-06 17:01:54 +0300

    create migration and model

---
+++ app/TransactionCategory_Payment.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class TransactionCategory_Payment extends Model
+{
+    //
+}
--- database/migrations/2018_07_06_135009_create_transaction_table.php
+++ database/migrations/2018_07_06_135009_create_transaction_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateTransactionTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,12 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('transaction', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->rememberToken();
+            $table->integer('transaction_category_id');
+            $table->integer('paymend_id');
+            $table->integer('action_id');
+            $table->integer('price');
             $table->timestamps();
         });
     }
@@ -30,6 +30,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('transaction');
     }
 }
