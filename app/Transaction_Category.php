648b47e89179be0f923244581f24afa1c7fac80e
itsaninho <itsaninho@gmail.com>
2018-07-06 17:55:11 +0300

    corect table name Transaction_Category

--- app/Transaction_Category.php
+++ app/Transaction_Category.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Transaction_Category extends Model
 {
-    protected $table = 'transaction_categoty';
+    protected $table = 'transaction_category';
 
     public function action()
     {
